0ab0063342e3314df0676da2874c1161de0dc95f
satogako <>
2023-06-26 16:21:54 +0000

    Add a link to each member on the About and add form to contact page

--- run.py
+++ run.py
@@ -11,12 +11,23 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about1") # що сюди введемо те і буде відображатися в кінці url адреси
+@app.route("/about") # що сюди введемо те і буде відображатися в кінці url адреси
 def about():
     data=[]
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member) # member - це ім’я змінної, яке передається до нашого member.html.
 
 
 @app.route("/contact")
